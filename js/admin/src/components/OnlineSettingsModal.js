5b419d0bcfaa15dfeb2b6e4737e01adf751958c4
Joel Bond <joel.bond@partialdev.com>
2018-04-21 20:46:34 -0600

    swapped the field types around

--- js/admin/src/components/OnlineSettingsModal.js
+++ js/admin/src/components/OnlineSettingsModal.js
@@ -13,9 +13,9 @@ export default class OnlineSettingsModal extends SettingsModal {
         return [
             <div className="Form-group">
                 <label>{app.translator.trans('antoinefr-online.admin.settings.titleoflist')}</label>
-                <input required className="FormControl" type="number" bidi={this.setting('antoinefr-online.titleoflist')}></input>
+                <input required className="FormControl" type="text" bidi={this.setting('antoinefr-online.titleoflist')}></input>
                 <label>{app.translator.trans('antoinefr-online.admin.settings.displaymax')}</label>
-                <input required className="FormControl" type="text" bidi={this.setting('antoinefr-online.displaymax')}></input>
+                <input required className="FormControl" type="number" bidi={this.setting('antoinefr-online.displaymax')}></input>
             </div>
         ];
     }
